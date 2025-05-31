3bac324b88018f55bfb9884d7d0e627998794ad4
DR1v3rs <DR1vers@icloud.com>
2025-06-01 00:06:43 +0400

    Перенёс считывание файла в класс авторизации сделал метод приватным подготовил ответы серверу

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.example.utils.FileUtils;
+import org.example.auth.Authorization;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,7 +8,7 @@ public class Main {
     public static String filePath = "C:\\Users\\DikushinAS\\IdeaProjects\\Auth_1.0\\Users.txt";
 
     public static void main(String[] args) {
-        FileUtils.processFile();
+        Authorization.userLogin("user05", "Pass01");
     }
 }
 
--- src/main/java/org/example/auth/Authorization.java
+++ src/main/java/org/example/auth/Authorization.java
@@ -1,19 +1,71 @@
 package org.example.auth;
-import org.example.utils.FileUtils;
 
+import org.example.Main;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class Authorization {
-    try {
-        Path Login = Path.of(FileUtils.matrix[1][1]);
+    private static boolean addUser = false;   //если True то запрос на добавление пользователя
+    private static boolean wrongPass = false; //если True то пароль неправильный
+    private static boolean login = false;     //если True то всё хорошо
 
+    private static void readFile(String user, String pass) {
+        try {
+            Path filePath = Path.of(Main.filePath);
+            List<String> lines = Files.readAllLines(filePath);
+            String[][] matrix = new String[lines.size()][];
 
+            for (int i = 0; i < lines.size(); i++) {
+                matrix[i] = lines.get(i).split(";");
+                if (lines.get(i) != null && matrix[i].length > 0 && matrix[i][0].equalsIgnoreCase(user)) {
+                    if (lines.get(i) != null && matrix[i].length > 0 && matrix[i][1].equals(pass)) {
+                        login = true;
+                        break;
+                    } else {
+                        wrongPass = true;
+                        }
+                    break;
+                } else {
+                    addUser = true;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error read file: " + e.getMessage());
+        }
+    }
 
- //   public boolean auth(String login, String password) {
-    //       System.out.println();
-//    }
+    public static boolean userLogin(String user, String pass) {
+        readFile(user, pass);
+        if (addUser) {
+            System.out.println("User not found! Please add user!");
+        } else if (login) {
+            System.out.println("User authorizated!!!");
+        } else if (wrongPass) {
+            System.out.println("Wrong password! Please try again!");
+        }
+
+        return addUser;
+    }
 }
 
+
+
+//public class Authorization {
+//    public static boolean checkLogin(String login) {
+//        String[][] credentials = FileUtils.getCredentials();
+
+//        for (String[] row : credentials) {
+//            System.out.println(Arrays.toString(row));
+//            if (row.length > 0 && row[0].equals(login)) {
+//                //System.out.println(credentials.toString(row));
+//                return true; // логин найден в первом столбце
+//            }
+//        }
+//        return false; // логин не найден
+//    }
+//}
 
 // метод регистрации пользователя (логин и пароль)
 
